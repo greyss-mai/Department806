835b6e47da1f896f6ce247960bb16cc13be1db9f
Shadeling <mironow.slav@yandex.ru>
2020-11-11 20:54:41 +0300

    Add files via upload

--- 2020/Mironov S.V Cuda/Cuda_2lab_число сочетаний.cu
+++ 2020/Mironov S.V Cuda/Cuda_2lab_число сочетаний.cu
@@ -14,57 +14,55 @@ __global__ void addKernel(int *inter, double *fact)
 	__shared__ double fact_gpu[4*3];
 	int i = threadIdx.x;
 	int num = int(i/4);
-  int i1 = i%4;
-  fact_gpu[i]=1;
+  	int i1 = i%4;
+  	fact_gpu[i]=1;
 
-  for(int j=inter[num*5+i1]; j<inter[num*5+i1+1]; j++){
-      fact_gpu[i]*=j;
-  }
+  	for(int j=inter[num*5+i1]; j<inter[num*5+i1+1]; j++){
+      		fact_gpu[i]*=j;
+  	}
 	__syncthreads();
-  fact[i]=fact_gpu[i];
+  	fact[i]=fact_gpu[i];
 }
 
 void calculate_gpu(int n, int k, int arraySize) {
 	
-  int intervals[5*3]; //5 интервала для n, 5 для k, 5 для n-k
-  double facts[(5-1)*3];
+  	int intervals[5*3]; //5 интервала для n, 5 для k, 5 для n-k
+  	double facts[(5-1)*3];
 
-  intervals[0] = 2; //начало
-  intervals[2] = int((2 + n) / 2); //середина
-  intervals[1] = int((2 + intervals[2]) / 2);
-  intervals[3] = int((intervals[2] + n) / 2);
-  intervals[4] = n+1;//конец
+  	intervals[0] = 2; //начало
+  	intervals[2] = int((2 + n) / 2); //середина
+  	intervals[1] = int((2 + intervals[2]) / 2);
+  	intervals[3] = int((intervals[2] + n) / 2);
+  	intervals[4] = n+1;//конец
   
-  intervals[0+arraySize] = 2; //начало
-  intervals[2+arraySize] = int((2 + k) / 2); //середина
-  intervals[1+arraySize] = int((2 + intervals[2+arraySize]) / 2);
-  intervals[3+arraySize] = int((intervals[2+arraySize] + k) / 2);
-  intervals[4+arraySize] = k+1;//конец
+  	intervals[0+arraySize] = 2; //начало
+  	intervals[2+arraySize] = int((2 + k) / 2); //середина
+  	intervals[1+arraySize] = int((2 + intervals[2+arraySize]) / 2);
+  	intervals[3+arraySize] = int((intervals[2+arraySize] + k) / 2);
+  	intervals[4+arraySize] = k+1;//конец
  
-  intervals[0+2*arraySize] = 2; //начало
-  intervals[2+2*arraySize] = int((2 + n-k) / 2); //середина
-  intervals[1+2*arraySize] = int((2 + intervals[2+2*arraySize]) / 2);
-  intervals[3+2*arraySize] = int((intervals[2+2*arraySize] + n-k) / 2);
-  intervals[4+2*arraySize] = n-k+1;//конец
+  	intervals[0+2*arraySize] = 2; //начало
+  	intervals[2+2*arraySize] = int((2 + n-k) / 2); //середина
+  	intervals[1+2*arraySize] = int((2 + intervals[2+2*arraySize]) / 2);
+  	intervals[3+2*arraySize] = int((intervals[2+2*arraySize] + n-k) / 2);
+  	intervals[4+2*arraySize] = n-k+1;//конец
 
 
-  auto begin = std::chrono::steady_clock::now();
+  	auto begin = std::chrono::steady_clock::now();
 	cudaError_t cudaStatus = addWithCuda(intervals, facts, arraySize);
 	if (cudaStatus != cudaSuccess) {
 		fprintf(stderr, "addWithCuda failed!");
 
 	}
-  auto end = std::chrono::steady_clock::now();
-  auto elapsed_ms = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+  	auto end = std::chrono::steady_clock::now();
+  	auto elapsed_ms = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
 	printf("Time= %i nanoseconds \n", elapsed_ms.count());
 
-  double nf = facts[0]*facts[1]*facts[2]*facts[3];
-  double kf = facts[0+4]*facts[1+4]*facts[2+4]*facts[3+4];
-  double nkf= facts[0+8]*facts[1+8]*facts[2+8]*facts[3+8];
-	double ans = nf/(kf*nkf);
-
-	//printf("n!=%lf  k!=%lf  n-k!=%lf \n", facts[0], facts[1], facts[2]);
-	printf("With GPU C (n=%i, k=%i) = %g \n", n, k, ans);
+  	double nf = facts[0]*facts[1]*facts[2]*facts[3];
+  	double kf = facts[0+4]*facts[1+4]*facts[2+4]*facts[3+4];
+  	double nkf= facts[0+8]*facts[1+8]*facts[2+8]*facts[3+8];
+  	double ans = nf/(kf*nkf);
+  	printf("With GPU C (n=%i, k=%i) = %g \n", n, k, ans);
 }
 
 double fact_cpu(int n) {
@@ -80,10 +78,10 @@ double fact_cpu(int n) {
 
 void calculate_cpu(int n, int k) {
 	auto begin = std::chrono::steady_clock::now();
-  double ans=fact_cpu(n)/(fact_cpu(k)*fact_cpu(n-k));
+  	double ans=fact_cpu(n)/(fact_cpu(k)*fact_cpu(n-k));
 	auto end = std::chrono::steady_clock::now();
 
-  auto elapsed_ms = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+  	auto elapsed_ms = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
 	printf("Time= %i nanoseconds \n", elapsed_ms.count());
 	printf("With CPU C (n=%i, k=%i) = %g \n", n, k, ans);
 	
@@ -92,18 +90,18 @@ void calculate_cpu(int n, int k) {
 int main()
 {
 	cudaError_t cudaStatus;
-  const int arraySize=5;
-  int n = 12;
-  int k = 4;
+  	const int arraySize=5;
+  	int n = 12;
+  	int k = 4;
 
 	for (int j = 0; j < 7; j++) {
-    calculate_cpu(n,k);
+    		calculate_cpu(n,k);
 		printf("\n");
 		calculate_gpu(n, k, arraySize);
-	  printf("\n");
+	  	printf("\n");
 
-    n*=2;
-    k*=2;
+    		n*=2;
+    		k*=2;
 	}
 
 	// cudaDeviceReset must be called before exiting in order for profiling and
