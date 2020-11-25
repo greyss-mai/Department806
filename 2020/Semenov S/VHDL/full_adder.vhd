b559ab32037b4038034c2fcecf5d93a5885cfa96
greyss-mai <greyss@mai.ru>
2020-11-25 18:28:32 +0300

    VHDL full adder

---
+++ 2020/Semenov S/VHDL/full_adder.vhd
@@ -0,0 +1,22 @@
+----------------------------------------------------------------------------------
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity full_adder is
+    Port ( a : in  STD_LOGIC;
+           b : in  STD_LOGIC;
+           cin : in  STD_LOGIC;
+           sum : out  STD_LOGIC;
+           cout : out  STD_LOGIC);
+end full_adder;
+
+architecture Behavioral of full_adder is
+
+begin
+
+sum <= a xor b xor cin;
+cout <= (a and b) or (a and cin) or (b and cin);
+
+end Behavioral;
+
