05165845327d8bd78e4bc89d24598de6cb9f1a25
greyss-mai <greyss@mai.ru>
2020-11-25 20:02:44 +0300

    Update counter.v

--- 2020/Semenov S/counter.v
+++ 2020/Semenov S/counter.v
@@ -1,22 +1,5 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    19:05:48 03/23/2020 
-// Design Name: 
-// Module Name:    counter 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
 //////////////////////////////////////////////////////////////////////////////////
 module counter(
     input CLOCK,
