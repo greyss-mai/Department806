16ba57552e2b04e7d63f6675996ff4d31dc2e2ed
greyss-mai <greyss@mai.ru>
2020-11-25 20:12:53 +0300

    Update full_adder.v

--- 2020/Semenov S/Verilog/full_adder.v
+++ 2020/Semenov S/Verilog/full_adder.v
@@ -1,22 +1,5 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    10:26:34 03/23/2020 
-// Design Name: 
-// Module Name:    full_adder 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
 //////////////////////////////////////////////////////////////////////////////////
 module full_adder(
     input a,
