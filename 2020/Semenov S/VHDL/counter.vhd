f424fee2f5e9f2bee8c9f4a3b7b699d0c4bf0828
greyss-mai <greyss@mai.ru>
2020-11-25 19:43:02 +0300

    Update counter.vhd

--- 2020/Semenov S/VHDL/counter.vhd
+++ 2020/Semenov S/VHDL/counter.vhd
@@ -1,35 +1,9 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    20:05:43 03/15/2020 
--- Design Name: 
--- Module Name:    counter - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity counter is
     Port ( CLOCK : in  STD_LOGIC;
