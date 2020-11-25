cdf51991d5676381e8df3bd72bd71e6c39c35d8d
greyss-mai <greyss@mai.ru>
2020-11-25 21:06:11 +0300

    Update add4.vhd

--- 2020/Semenov S/VHDL/add4.vhd
+++ 2020/Semenov S/VHDL/add4.vhd
@@ -1,33 +1,7 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    17:36:18 11/25/2020 
--- Design Name: 
--- Module Name:    add4 - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity add4 is
     Port ( a : in  STD_LOGIC_VECTOR (3 downto 0);
