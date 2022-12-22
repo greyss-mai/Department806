8ebdb8cf580d948b74224897f53ffc1805fff909
greyss-mai <greyss@mai.ru>
2022-12-22 14:09:18 +0300

    Insertion sort program

---
+++ 2022/SemenovS/C/insertionSort/README.md
@@ -0,0 +1,21 @@
+# insertionSort - a program that implements Insertion sort algorithm.
+# insertionSort - программа, которая реализует алгоритм сортировки вставками.
+
+## Description
+
+Insertion sort is a simple sorting algorithm that builds the final sorted array (or list) one item at a time by comparisons.
+The size of the sort array is set in the source code.
+
+Алгоритм сортировки, в котором элементы входной последовательности просматриваются по одному,
+и каждый новый поступивший элемент размещается в подходящее место среди ранее упорядоченных элементов.
+Размер массива для сортировки задаётся в коде.
+## Supported OSes
+
+Windows
+
+## Build and Run
+
+### Windows
+The Windows samples are built using the Visual Studio IDE.
+
+Программа построена в Visual Studio IDE.
---
+++ 2022/SemenovS/C/insertionSort/insertionSort.cpp
@@ -0,0 +1,45 @@
+﻿// insertionSort.cpp : программа выполняет сортировку вставками. 
+// Функция generate создаёт массив случайных чисел
+// Функция insertionSort сортирует массив
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void generate(int *array, int n) {
+    for (int i = 0; i < n; i++) array[i] = rand();
+}
+
+void insertionSort(int* array, int n) {
+    for (int i = 1; i < n; i++) {
+        int j = i;
+        int temp = array[j];
+        while (array[j-1]>temp && j>0) {
+            array[j] = array[j-1];
+            --j;
+        }
+        array[j] = temp;
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Program main
+////////////////////////////////////////////////////////////////////////////////
+int main(int argc, char **argv){
+    int n = 10;
+    int *array = new int[n];
+
+    generate(array, n);
+
+    //output to the screen
+    for (int i = 0; i < n; i++) printf("%d ", array[i]);
+
+    insertionSort(array, n);
+
+    //output to the screen
+    printf("\n");
+    for (int i = 0; i < n; i++) printf("%d ", array[i]);
+    
+    delete[] array;
+    system("pause");
+    return 0;
+}
